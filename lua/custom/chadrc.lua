5e462baef9e309d02115c98d21324f36d5caad73
袁苏义 <yuansuyi@hzdusun.com>
2023-12-13 17:04:48 +0800

    change visual mode selected text backgroud color

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -6,6 +6,14 @@ M.ui = {
   nvdash = {
     load_on_startup = true
   },
+  hl_override = {
+        Visual = {
+             bg = {"grey", 2}
+        },
+        -- Normal = {
+        --     bg = {"black", 0},
+        -- }
+    }
 }
 M.plugins = 'custom.plugins'
 M.mappings = require 'custom.mappings'
